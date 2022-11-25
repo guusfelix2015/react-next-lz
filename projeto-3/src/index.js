83310e4b9b5fd03a2b2b8c1ff55cff839e584678
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-24 22:56:26 -0300

    Iniciando no styled components

--- projeto-3/src/index.js
+++ projeto-3/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './styles/global-styles';
 
 import Home from './templates/App/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>,
 );

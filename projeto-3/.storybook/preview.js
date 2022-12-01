d4e0ce7e7ba6230d1edc19cce28fd89cdbabd3e6
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-30 22:03:33 -0300

    Configurando storybook

--- projeto-3/.storybook/preview.js
+++ projeto-3/.storybook/preview.js
@@ -4,11 +4,18 @@ import { theme } from '../src/styles/theme';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ],
   },
 };
 
